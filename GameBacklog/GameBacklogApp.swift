6d6d21187f17a010855461a6ce69a61c36d6280d
HunterX85 <hunter8569@gmail.com>
2026-05-02 17:52:27 +0300

    Initial commit

---
+++ GameBacklog/GameBacklogApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameBacklogApp.swift
+//  GameBacklog
+//
+//  Created by Serhii Pershuta on 02.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct GameBacklogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
